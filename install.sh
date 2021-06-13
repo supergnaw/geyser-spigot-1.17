46975c8edd87d06f5170f2b08dae28f4e103e6ba
supergnaw <loren.supernaw@gmail.com>
2021-06-13 14:05:45 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 # Update System & Utilities
-sudo yum remove java*
-sudo yum update -y
-sudo dnf install epel-release -y
-sudo dnf install screen -y
-sudo yum install git iptables -y
+yum remove java*
+yum update -y
+dnf install epel-release -y
+dnf install screen -y
+yum install git iptables -y
 
 # Install Java
 #wget -O jdk-16.0.1_linux-x64_bin.rpm https://download.oracle.com/otn-pub/java/jdk/16.0.1+9/7147401fd7354114ac51ef3e1328291f/jdk-16.0.1_linux-x64_bin.rpm
-wget --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" https://download.oracle.com/otn-pub/java/jdk/16.0.1+9/7147401fd7354114ac51ef3e1328291f/jdk-16.0.1_linux-x64_bin.rpm
+#wget --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" https://download.oracle.com/otn-pub/java/jdk/16.0.1+9/7147401fd7354114ac51ef3e1328291f/jdk-16.0.1_linux-x64_bin.rpm
 #wget --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" https://download.oracle.com/otn-pub/java/jdk/15.0.1+9/51f4f36ad4ef43e39d0dfdbaf6549e32/jdk-15.0.1_linux-x64_bin.rpm
-sudo rpm -ivh jdk-16.0.1_linux-x64_bin.rpm
-#sudo yum install -y git java-16.0.0-openjdk-devel
-#sudo yum install -y git java-16*
+#rpm -ivh jdk-16.0.1_linux-x64_bin.rpm
+#yum install -y git java-16.0.0-openjdk-devel
+#yum install -y git java-16*
 
 # Create BuildTools Directory
 mkdir BuildTools
@@ -34,7 +34,8 @@ cd ./plugins
 wget -O Geyser-Spigot.jar https://ci.opencollab.dev//job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/spigot/target/Geyser-Spigot.jar
 
 # Startup Script
-echo screen -dmS "spigot" java -Xmx3G -Xms3G -jar Server/spigot-1.17.jar nogui > Server/startup.sh
+touch Server/startup.sh
+echo screen -dmS "spigot" java -Xmx3G -Xms3G -jar spigot-1.17.jar nogui > Server/startup.sh
 
 # Firewall Rules
 iptables -A INPUT -p tcp --dport 25565 -j ACCEPT --comment "Minecraft Java" >> Server/Startup.sh
@@ -42,3 +43,6 @@ iptables -A INPUT -p tcp --dport 19132 -j ACCEPT --comment "Minecraft Bedrock" >
 iptables -P INPUT DROP
 iptables -P FORWARD DROP
 iptables -P OUTPUT ACCEPT
+
+# Ownership
+chown -R spigot:spigot .
