29b499b40df2aac80defda9d75397795178128f5
supergnaw <loren.supernaw@gmail.com>
2021-06-13 12:19:50 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,8 @@ sudo rpm -ivh jdk-16.0.1_linux-x64_bin.rpm
 #sudo yum install -y git java-16*
 
 # Create BuildTools Directory
-mkdir BuildTools && cd BuildTools
+mkdir BuildTools
+cd BuildTools
 
 # Download BuildTools
 wget -O BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar 
@@ -24,10 +25,12 @@ git config --global --unset core.autocrlf
 java -jar BuildTools.jar --rev 1.17
 
 # Prepare Server Directory
-mkdir ../Server && cd ../Server
+mkdir ../Server
+cd ../Server
 cp ../BuildTools/spigot-1.17.jar ./
 echo eula=true > eula.txt
-mkdir ./plugins && cd ./plugins
+mkdir ./plugins
+cd ./plugins
 wget -O Geyser-Spigot.jar https://ci.opencollab.dev//job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/spigot/target/Geyser-Spigot.jar
 
 # Startup Script
